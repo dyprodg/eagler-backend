ead99639e8ddb53d02722b377c0a31c29cc00038
Dennis <dennisdiepolder@outlook.com>
2024-01-30 22:47:03 +0100

    function change to just compress file

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -1,39 +1,9 @@
 import boto3
-from PIL import Image, ImageResampling
+from PIL import Image
 from io import BytesIO
 import os
 
 s3_client = boto3.client('s3')
-
-def resize_and_crop(image, target_width, target_height):
-    # Berechnen des Seitenverhältnisses des Zielbildes
-    target_ratio = target_width / target_height
-
-    # Berechnen des Seitenverhältnisses des Originalbildes
-    original_width, original_height = image.size
-    original_ratio = original_width / original_height
-
-    # Bestimmen, ob das Bild auf Breite oder Höhe skaliert werden soll
-    if target_ratio > original_ratio:
-        # Bild wird auf die Zielbreite skaliert
-        scaled_width = target_width
-        scaled_height = round(target_width / original_ratio)
-    else:
-        # Bild wird auf die Zielhöhe skaliert
-        scaled_height = target_height
-        scaled_width = round(target_height * original_ratio)
-
-    # Skalieren des Bildes
-    image = image.resize((scaled_width, scaled_height), ImageResampling.LANCZOS)
-
-    # Zentrieren und Ausschneiden des Bildes
-    left = (scaled_width - target_width) / 2
-    top = (scaled_height - target_height) / 2
-    right = (scaled_width + target_width) / 2
-    bottom = (scaled_height + target_height) / 2
-
-    image = image.crop((left, top, right, bottom))
-    return image
 
 def lambda_handler(event, context):
     source_bucket = event['Records'][0]['s3']['bucket']['name']
@@ -43,16 +13,16 @@ def lambda_handler(event, context):
     file = s3_client.get_object(Bucket=source_bucket, Key=key)
     file_content = file['Body'].read()
 
-    # Image processing: Crop and compress
+    # Image processing: Open and convert to JPEG
     image = Image.open(BytesIO(file_content))
-    image = resize_and_crop(image, 1080, 1080)  # Zuschneiden und Skalieren
 
-    # Konvertieren des Bildes in RGB
-    image = image.convert('RGB')
+    # Konvertieren Sie das Bild in RGB, falls es RGBA oder ein anderes Format ist
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
 
-    # Save the processed image temporarily
+    # Compress the image
     buffer = BytesIO()
-    image.save(buffer, 'JPEG')  # Speichere alle Bilder als JPEG
+    image.save(buffer, 'JPEG', quality=85)  # Qualität auf 85 setzen, um die Dateigröße zu reduzieren
     buffer.seek(0)
 
     # Determine the filename for the target bucket
